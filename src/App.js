0457cb8f230087bb2c51a49e25efcee919e6e6ae
EC2 Default User <ec2-user@ip-172-31-20-183.eu-west-3.compute.internal>
2024-06-14 19:38:06 +0000

    check CI/CD with security end-to-end

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 3 (Andras Nagy)
+            EKS DEMO Blog version 4 CI/CD with Security (Andras Nagy)
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
